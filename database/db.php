1c3fd19354741b3e0c0f2fb67364315b86d078b0
Luis-Mocha <luismocha97@gmail.com>
2023-06-04 00:07:28 +0200

    array

--- database/db.php
+++ database/db.php
@@ -5,8 +5,8 @@ include_once __DIR__ . '/../models/genre.php';
 
 $arrayMovies = [
     new Movie('spider-man', 5, [ new Genre('cinecomic'), new Genre('action') ] ),
-    new Movie('billy elliot', 5, new Genre('drama') ),
-    new Movie('the conjuring', 5, new Genre('horror') ),
+    new Movie('billy elliot', 5, [ new Genre('drama') ] ),
+    new Movie('the conjuring', 5, [ new Genre('horror') ] ),
 ]
 
 
