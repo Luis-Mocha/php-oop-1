c5b78957ada7a54e9c4c977d009a320fb7a0b2e7
Luis-Mocha <luismocha97@gmail.com>
2023-06-03 21:35:06 +0200

    funzione setter

--- index.php
+++ index.php
@@ -9,10 +9,19 @@ class Movie {
         $this->genre = $_genre;
         $this->vote = $_vote;
     }
+
+    //metodo setter
+    function setVote($_vote){
+        $this->vote = $_vote;
+    }
 }
 
 $spider_man = new Movie('spider-man', 'cinecomic', 5);
 
+var_dump($spider_man);
+
+//richiamo la funzione setter della calsse Movie
+$spider_man->setVote(4);
 var_dump($spider_man);
 
 
