cae7cc8b6095e8132dddeeff75a9c3e1033b09c2
Luis-Mocha <luismocha97@gmail.com>
2023-06-04 00:29:21 +0200

    elimino commenti superflui

--- index.php
+++ index.php
@@ -2,34 +2,8 @@
 
 include_once __DIR__ . '/database/db.php';
 
-
-/*
-$spider_man = new Movie('spider-man', 4, new Genre('cinecomic', 'action') );
-
-var_dump($spider_man);
-
-//richiamo la funzione setter della classe Movie
-$spider_man->setVote(5);
-var_dump($spider_man);
-
-
-echo $spider_man->genre->secondary_genre;
-
-//richiamo la funzione setter della classe Genre
-$spider_man->genre->setSecondary('adventure');
-var_dump($spider_man);
-
-$elliot = new Movie('billy elliot', 5, new Genre('drama', null) );
-var_dump($elliot);
-*/
-
 var_dump($arrayMovies);
 
-// foreach ($arrayMovies as $elem ) {
-//     foreach ($elem->genre as $subElem) {
-//         echo $subElem->name;
-//     }
-// }
 
 include_once __DIR__ . '/partials/htmlTop.php';
 
