7affaaa193d77ffb3dd7aa1fa646c4898fb1c511
Luis-Mocha <luismocha97@gmail.com>
2023-06-03 22:17:01 +0200

    seconda istanza classe Movie

--- index.php
+++ index.php
@@ -5,13 +5,22 @@ include_once __DIR__ . '/models/genre.php';
 
 
 
-$spider_man = new Movie('spider-man', 5, new Genre('cinecomic', 'action') );
+$spider_man = new Movie('spider-man', 4, new Genre('cinecomic', 'action') );
 
 var_dump($spider_man);
 
 //richiamo la funzione setter della classe Movie
-$spider_man->setVote(4);
+$spider_man->setVote(5);
 var_dump($spider_man);
 
+
+echo $spider_man->genre->secondary_genre;
+
+//richiamo la funzione setter della classe Genre
+$spider_man->genre->setSecondary('adventure');
+var_dump($spider_man);
+
+$elliot = new Movie('billy elliot', 5, new Genre('drama', null) );
+var_dump($elliot);
 
 ?>
