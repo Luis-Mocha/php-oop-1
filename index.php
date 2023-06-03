9a537e4a80833676239b218fa41c6d88fecd8509
Luis-Mocha <luismocha97@gmail.com>
2023-06-03 21:30:05 +0200

    funzione "constructor"

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 class Movie {
     public $title;
     public $genre;
-    public $release_date;
     public $vote;
 
+    function __construct( $_title, $_genre, $_vote) {
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->vote = $_vote;
+    }
 }
 
-$spider_man = new Movie();
+$spider_man = new Movie('spider-man', 'cinecomic', 5);
 
 var_dump($spider_man);
 
