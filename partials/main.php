74857b1bbf2cbf0f7814e93d14d713bf75ecaf06
Luis-Mocha <luismocha97@gmail.com>
2023-06-04 00:40:05 +0200

    main section cards

--- partials/main.php
+++ partials/main.php
@@ -1,7 +1,37 @@
 <main class="bg-secondary">
     <div class="container">
-        <h1>
-            Prova
-        </h1>
+        
+        <?php foreach ($arrayMovies as $elem) { ?>
+
+            <div class="card text-center">
+
+                <!-- TITOLO -->
+                <div class="card-title">
+                    <?php echo $elem->title ?>
+                </div>
+
+                <!-- VOTO -->
+                <div class="card-text">
+                    <span>
+                        Voto: <?php echo $elem->vote ?> / 5
+                    </span>
+                </div>
+
+                <!-- GENERE -->
+                <div class="card-text">
+                    <span>
+                        Genere:
+                        <?php foreach ($elem->genre as $elemGenre) { ?>
+                            
+                            <li> <?php echo $elemGenre->name ?> </li>
+
+                        <?php } ?>
+                    </span>
+                </div>
+
+            </div>
+
+        <?php } ?>
+
     </div>
 </main>
